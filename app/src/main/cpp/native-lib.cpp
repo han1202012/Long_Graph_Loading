1be5fd5d705453eae36ff29e235eeb31c3095bda
han1202012 <octopus_truth@163.com>
2020-07-03 13:02:11 +0800

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,9 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_kim_hsl_long_1graph_1loading_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_kim_hsl_lgl_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
