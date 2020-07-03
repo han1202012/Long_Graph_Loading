dcc40a5aa2a72ef65cd202b2953e0ecf355a6bf2
octopus <octopus_truth@163.com>
2020-07-03 11:50:08 +0800

    Initial commit

---
+++ app/src/main/java/kim/hsl/long_graph_loading/MainActivity.java
@@ -0,0 +1,24 @@
+package kim.hsl.long_graph_loading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    public native String stringFromJNI();
+}
